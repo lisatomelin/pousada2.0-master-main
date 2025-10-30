55a71a1a027b030bec279afaf23a9ca39d9ac965
Leandro dos Santos <51712356+leandro-d-santos@users.noreply.github.com>
2025-10-30 06:38:24 -0300

    add test files

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,29 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1'},
+
+  // Transforma arquivos TS e templates HTML
   transform: {
-    '^.+\\.(ts|mjs|html|js)$': 'ts-jest',
+    '^.+\\.(ts|mjs|html|js)$': 'jest-preset-angular',
   },
+
+  // Ignora transformações de dependências específicas
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$)'
+  ],
+
+  // Faz Jest entender imports como src/ ou @app/
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/' }),
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss)$': 'identity-obj-proxy', // ignora estilos
+    '\\.(html)$': '<rootDir>/__mocks__/htmlMock.js', // ignora HTML externo
+  },
+
+  testEnvironment: 'jsdom',
+
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
-  testMatch: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts'],
-  globals: {},
 };
--- setup-jest.ts
+++ setup-jest.ts
@@ -1,5 +1,5 @@
-
-
 import 'jest-preset-angular/setup-jest';
 
-
+import { TextEncoder, TextDecoder } from 'util';
+(global as any).TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
